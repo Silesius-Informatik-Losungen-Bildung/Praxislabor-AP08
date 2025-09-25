39365f722a8d6438510d02f42e1cb30c0c2d44f1
Patrik Hartmann <ptrkhartmann@gmail.com>
2025-09-25 15:57:05 +0200

    WebApp create

---
+++ Ressourcen/backendConsole/StempelApp/Controllers/StempelAppController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StempelApp.WebControllers
+{
+    public class StempelAppController : Controller
+    {
+        private readonly ILogger<StempelAppController> _logger;
+
+        public StempelAppController(ILogger<StempelAppController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult UserHomepage()
+        {
+            return View();
+        }
+    }
+}
