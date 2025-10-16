293a3b9a59e14e669b183516d2f59a42f760d622
Enma1111 <bartalsky66@gmail.com>
2025-10-16 13:57:04 +0200

    Sehr viel Typescript

---
+++ Ressourcen/backendConsole/StempelApp/Scripts/index.ts
@@ -0,0 +1,9 @@
+﻿export * from './global/config.js';
+export * from './global/api.js';
+export * from './global/ui.js';
+
+import { initUI } from './global/ui.js';
+
+document.addEventListener("DOMContentLoaded", () => {
+    initUI();
+});
