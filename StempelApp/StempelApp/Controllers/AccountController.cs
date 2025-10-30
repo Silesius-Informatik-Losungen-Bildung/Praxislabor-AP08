982dd9550e0218651e6e5fb5ae57338673a0cdfb
Roman <122753048+Moavalon@users.noreply.github.com>
2025-10-30 15:55:51 +0100

    kleine änderung

--- StempelApp/StempelApp/Controllers/AccountController.cs
+++ StempelApp/StempelApp/Controllers/AccountController.cs
@@ -51,7 +51,7 @@ namespace StempelApp.Controllers
                 newPassword = model.Password
             };
 
-            var response = await _httpClient.PostAsJsonAsync("Accountapi/set-password", setPasswordData);
+            var response = await _httpClient.PostAsJsonAsync("http://localhost:5209/accountapi/set-password", setPasswordData);
 
             if (response.IsSuccessStatusCode)
             {
