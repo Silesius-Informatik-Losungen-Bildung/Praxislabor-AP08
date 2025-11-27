b6d2a42c6bd3a49a2f2f7502d6c525f8c1aa4560
Roman <122753048+Moavalon@users.noreply.github.com>
2025-11-27 13:34:30 +0100

    resetpasswordviewmodel erstellt

---
+++ StempelApp/StempelApp/Viewmodels/ResetPasswordViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StempelApp.Viewmodels
+{
+    public class ResetPasswordViewModel
+    {
+        [EmailAddress(ErrorMessage = "Ungültige E-Mail-Adresse.")]
+        [Required(ErrorMessage = "E-Mail-Adresse ist erforderlich.")]
+        public string Email { get; set; } = null!;
+    }
+}
