e37a066f36ce1019a7dee0e599dd0d8bf45f389f
Roman Gutmann <ITN272221@qualifizierung.at>
2025-12-05 11:01:21 +0100

    versuch authorize

--- StempelApp/StempelApp/Controllers/AccountController.cs
+++ StempelApp/StempelApp/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using StempelApp.Viewmodels;
 using System.Text.Json;
 
@@ -194,6 +195,7 @@ namespace StempelApp.Controllers
 
 
         [HttpGet]
+        //[Authorize]
         public IActionResult Dashboard()
         {
             return View();
