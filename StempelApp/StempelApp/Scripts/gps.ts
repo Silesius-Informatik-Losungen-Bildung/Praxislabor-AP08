0a522e62fd2a7b8a1668e4207184783c5459c784
Patrik Hartmann <ITN280565@qualifizierung.at>
2026-01-14 11:01:16 +0100

    add from fronted

---
+++ StempelApp/StempelApp/Scripts/gps.ts
@@ -0,0 +1,23 @@
+﻿export interface GpsData {
+    latitude: number;
+    longitude: number;
+}
+
+export async function getLocation(): Promise<GpsData> {
+    return new Promise((resolve, reject) => {
+        if (!navigator.geolocation) {
+            reject(new Error("Geolocation nicht unterstützt"));
+            return;
+        }
+
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                resolve({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                });
+            },
+            (error) => reject(error)
+        );
+    });
+}
